d5cef35458ed2d12ce774b8f52d5e810a3e73690
Nemo.Yan <nemo.yan@tbwa.com>
2021-08-18 13:43:51 +0800

    add threeJs and a simple example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="hello" id="container">
+    <h1></h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,10 +31,48 @@
 </template>
 
 <script>
+import * as Three from 'three'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'ThreeTest',
+  data() {
+    return {
+      camera: null,
+      scene: null,
+      renderer: null,
+      mesh: null
+    }
+  },
+  methods: {
+    init: function() {
+        let container = document.getElementById('container');
+
+        this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
+        this.camera.position.z = 1;
+
+        this.scene = new Three.Scene();
+
+        let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
+        let material = new Three.MeshNormalMaterial();
+
+        this.mesh = new Three.Mesh(geometry, material);
+        this.scene.add(this.mesh);
+
+        this.renderer = new Three.WebGLRenderer({antialias: true});
+        this.renderer.setSize(container.clientWidth, container.clientHeight);
+        container.appendChild(this.renderer.domElement);
+
+    },
+    animate: function() {
+        requestAnimationFrame(this.animate);
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+        this.renderer.render(this.scene, this.camera);
+    }
+  },
+  mounted() {
+      this.init();
+      this.animate();
   }
 }
 </script>
